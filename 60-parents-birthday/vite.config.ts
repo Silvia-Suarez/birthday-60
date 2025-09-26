000614ea669cb36b5d437d175f68b0a63c515785
silvia-suarez <silviznatalia@gmail.com>
2025-09-26 18:47:11 -0500

    git hub pages and backend updated

--- 60-parents-birthday/vite.config.ts
+++ 60-parents-birthday/vite.config.ts
@@ -2,11 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  // IMPORTANT: Set the correct base when deploying to GitHub Pages
-  // Replace with your repository name if different
-  base: "/60-parents-birthday/",
+  base: "/birthday-60/",
   plugins: [
     react(),
     tailwindcss()
